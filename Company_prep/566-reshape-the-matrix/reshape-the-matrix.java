9ae1e5207e85137867418a158c98d882e577e95b
kanishkhaa <151703688+kanishkhaa@users.noreply.github.com>
2026-01-15 10:51:33 +0530

    Time: 0 ms (100.00%) | Memory: 47.1 MB (95.46%) - LeetSync

--- Company_prep/566-reshape-the-matrix/reshape-the-matrix.java
+++ Company_prep/566-reshape-the-matrix/reshape-the-matrix.java
@@ -9,9 +9,7 @@ class Solution {
         int index = 0;
         for(int i=0;i<n;i++) {
             for(int j=0;j<m;j++) {
-                int newrow = index/c;
-                int newcol = index%c;
-                res[newrow][newcol] = mat[i][j];
+                res[index/c][index%c] = mat[i][j];
                 index+=1;
             }
         }
