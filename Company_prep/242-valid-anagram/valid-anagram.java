c16af06835764178ee6d0e413b91110a7aa0e205
kanishkhaa <151703688+kanishkhaa@users.noreply.github.com>
2026-01-16 09:35:11 +0530

    Time: 4 ms (79.30%) | Memory: 46.1 MB (49.14%) - LeetSync

--- Company_prep/242-valid-anagram/valid-anagram.java
+++ Company_prep/242-valid-anagram/valid-anagram.java
@@ -1,6 +1,5 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length()!= t.length()) return false;
         char[] s1 = s.toCharArray();
         char[] s2 = t.toCharArray();
         Arrays.sort(s1);
