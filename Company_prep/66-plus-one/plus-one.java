b21bb3986194ca3201815a411f991de3cbf4bdf7
kanishkhaa <151703688+kanishkhaa@users.noreply.github.com>
2026-01-19 20:53:17 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (51.33%) - LeetSync

--- Company_prep/66-plus-one/plus-one.java
+++ Company_prep/66-plus-one/plus-one.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] plusOne(int[] digits) {
         for(int i=digits.length-1;i>=0;i--) {
-            if(digits[i]+1!=10){
+            if(digits[i]+1!=10) {
                 digits[i]+=1;
                 return digits;
             }
