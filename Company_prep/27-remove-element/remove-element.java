739da59e862079504d74b597e112d849139e7f57
kanishkhaa <151703688+kanishkhaa@users.noreply.github.com>
2026-02-02 13:28:38 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (18.00%) - LeetSync

--- Company_prep/27-remove-element/remove-element.java
+++ Company_prep/27-remove-element/remove-element.java
@@ -1,8 +1,8 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
         int slow = 0;
-        for(int fast = 0;fast<nums.length;fast++) {
-            if(nums[fast]!=val) {
+        for(int fast=0;fast<nums.length;fast++) {
+            if(nums[fast]!= val) {
                 nums[slow] = nums[fast];
                 slow++;
             }
