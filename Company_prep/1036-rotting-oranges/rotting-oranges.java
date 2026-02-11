07bd51615464f253ba3e3f46920f2363095ce189
kanishkhaa <151703688+kanishkhaa@users.noreply.github.com>
2026-02-11 09:23:39 +0530

    Time: 2 ms (91.18%) | Memory: 44.6 MB (10.89%) - LeetSync

--- Company_prep/1036-rotting-oranges/rotting-oranges.java
+++ Company_prep/1036-rotting-oranges/rotting-oranges.java
@@ -19,13 +19,9 @@ class Solution {
 
         Queue<Pair> q = new LinkedList<>();
         int[][] vis = new int[n][m];
-
         int fresh = 0;
-
-        // ----- Initial scan -----
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
-
                 if(grid[i][j] == 2) {
                     q.add(new Pair(i, j, 0));
                     vis[i][j] = 2;
@@ -33,51 +29,37 @@ class Solution {
                 else {
                     vis[i][j] = 0;
                 }
-
                 if(grid[i][j] == 1)
-                    fresh++;
+                  fresh++;
             }
         }
 
         int time = 0;
         int rotten = 0;
 
-        int[] drow = {-1,0,1,0};
+        int[] drow = {-1,0,1,0}; //(row-1,col = top , row, col+1 = right, row+1,col = bottom, row,col-1 = left)
         int[] dcol = {0,1,0,-1};
-
-        // ----- BFS -----
         while(!q.isEmpty()) {
-
             Pair p = q.poll();
-
             int r = p.row;
             int col = p.col;
             int t = p.time;
-
             time = Math.max(time, t);
-
             for(int i = 0; i < 4; i++) {
-
                 int nrow = r + drow[i];
                 int ncol = col + dcol[i];
-
                 if(nrow >= 0 && nrow < n &&
-                   ncol >= 0 && ncol < m &&
-                   grid[nrow][ncol] == 1 &&
-                   vis[nrow][ncol] == 0) {
-
-                    q.add(new Pair(nrow, ncol, t + 1));
-                    vis[nrow][ncol] = 2;
-
+                   ncol >= 0 && ncol < m && // to check within limit
+                   grid[nrow][ncol] == 1 && // to check whether they are fresh
+                   vis[nrow][ncol] == 0) { // to check whether they are unvisited
+                    q.add(new Pair(nrow, ncol, t + 1)); // add the pair and increase time
+                    vis[nrow][ncol] = 2; // mark them rotten
                     rotten++;
                 }
             }
         }
-
-        // ----- Final Check -----
-        if(rotten != fresh)
+        if(rotten != fresh) // to check whether all oranges are rotten if not return -1
             return -1;
-
         return time;
     }
 }
