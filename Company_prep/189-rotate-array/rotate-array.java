969dace177de7254647049c40a8f1eec168ff796
kanishkhaa <151703688+kanishkhaa@users.noreply.github.com>
2026-03-02 19:40:22 +0530

    Time: 0 ms (100.00%) | Memory: 61 MB (96.53%) - LeetSync

--- Company_prep/189-rotate-array/rotate-array.java
+++ Company_prep/189-rotate-array/rotate-array.java
@@ -1,5 +1,5 @@
 class Solution {
-    public void rot(int[] nums, int start, int end) {
+    public void rev(int[] nums, int start, int end) {
         while(start<end) {
             int temp = nums[start];
             nums[start] = nums[end];
@@ -9,10 +9,10 @@ class Solution {
         }
     }
     public void rotate(int[] nums, int k) {
-        int n = nums.length;
-        k = k%n;
-        rot(nums,0,n-1);
-        rot(nums,0,k-1);
-        rot(nums,k,n-1);
+        int n =  nums.length;
+        k = k % n;
+        rev(nums,0,n-1);
+        rev(nums,0,k-1);
+        rev(nums,k,n-1);
     }
 }
