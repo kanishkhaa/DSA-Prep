d1d514fd3d4c2bfd8c3aa8ce7402ab2257622d85
kanishkhaa <151703688+kanishkhaa@users.noreply.github.com>
2026-03-12 02:29:00 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (35.98%) - LeetSync

--- Company_prep/1310-watering-plants/watering-plants.java
+++ Company_prep/1310-watering-plants/watering-plants.java
@@ -4,11 +4,11 @@ class Solution {
         int water = capacity;
         for(int i=0;i<plants.length;i++) {
             if(water<plants[i]) {
-                steps += i*2;
+                steps+=i*2;
                 water = capacity;
             }
             water-=plants[i];
-            steps+=1;
+            steps++;
         }
         return steps;
     }
