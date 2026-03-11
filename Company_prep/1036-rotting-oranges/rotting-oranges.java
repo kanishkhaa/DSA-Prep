baa93fdc16a3fa0c539c86c512bfddbcd5fe14ba
kanishkhaa <151703688+kanishkhaa@users.noreply.github.com>
2026-03-11 16:01:20 +0530

    Time: 2 ms (90.93%) | Memory: 43.9 MB (90.90%) - LeetSync

--- Company_prep/1036-rotting-oranges/rotting-oranges.java
+++ Company_prep/1036-rotting-oranges/rotting-oranges.java
@@ -1,38 +1,70 @@
+import java.util.*;
+
 class Solution {
     public int orangesRotting(int[][] grid) {
+
         int rows = grid.length;
         int cols = grid[0].length;
+
         Queue<int[]> queue = new LinkedList<>();
         int fresh = 0;
-        for(int i=0;i<rows;i++) {
-            for(int j=0;j<cols;j++) {
-                if(grid[i][j] == 2) {
-                    queue.add(new int[]{i,j});
+
+        // Step 1: find rotten oranges and count fresh
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+
+                if(grid[i][j]==2){
+                    queue.offer(new int[]{i,j});
                 }
-                if(grid[i][j] == 1) {
+
+                if(grid[i][j]==1){
                     fresh++;
                 }
             }
         }
-        if(fresh==0) return 0;
+
+        // If no fresh oranges
+        if(fresh==0)
+            return 0;
+
         int minutes = 0;
-        int[][] directions = {{1,0},{-1,0},{0,1},{0,-1}};
-        while(!queue.isEmpty() && fresh>0) {
+
+        int[][] directions = {
+                {1,0}, {-1,0}, {0,1}, {0,-1}
+        };
+
+        // BFS
+        while(!queue.isEmpty()){
+
             int size = queue.size();
-            minutes++;
-            for(int i=0;i<size;i++) {
-                int[] curr = queue.poll();
-                for(int[] dir : directions) {
-                    int r = curr[0] + dir[0];
-                    int c = curr[1] + dir[1];
-                    if(r>=0 && r<rows && c>=0 && c<cols && grid[r][c] == 1) {
-                        grid[r][c] = 2;
+            boolean infected = false;
+
+            for(int i=0;i<size;i++){
+
+                int[] cell = queue.poll();
+                int r = cell[0];
+                int c = cell[1];
+
+                for(int[] dir : directions){
+
+                    int nr = r + dir[0];
+                    int nc = c + dir[1];
+
+                    if(nr>=0 && nc>=0 && nr<rows && nc<cols && grid[nr][nc]==1){
+
+                        grid[nr][nc] = 2;
                         fresh--;
-                        queue.add(new int[]{r,c});
+
+                        queue.offer(new int[]{nr,nc});
+                        infected = true;
                     }
                 }
             }
+
+            if(infected)
+                minutes++;
         }
-        return fresh == 0? minutes : -1;
+
+        return fresh==0 ? minutes : -1;
     }
 }
