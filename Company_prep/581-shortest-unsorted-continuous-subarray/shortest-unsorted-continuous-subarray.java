1a212a0117d90de0d02d3ce75ced5a7f02d27235
kanishkhaa <151703688+kanishkhaa@users.noreply.github.com>
2026-03-21 10:04:41 +0530

    Time: 12 ms (26.59%) | Memory: 48 MB (17.47%) - LeetSync

--- Company_prep/581-shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.java
+++ Company_prep/581-shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.java
@@ -6,10 +6,10 @@ class Solution {
         int end = 0;
         for(int i=0;i<nums.length;i++) {
             if(nums[i] != sorted[i]) {
-                 start = Math.min(start,i);
-                 end = Math.max(end,i);
-            }
+            start = Math.min(start,i);
+            end = Math.max(end,i);
+          }
         }
-        return (end-start >= 0) ? (end-start+1) : 0;
+        return ((end-start>=0) ? (end-start+1) : 0);
     }
 }
