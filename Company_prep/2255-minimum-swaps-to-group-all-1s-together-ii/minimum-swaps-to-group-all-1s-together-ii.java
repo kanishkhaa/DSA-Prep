9dd3a94d19527fc7bd307ce6943ee2799540de31
kanishkhaa <151703688+kanishkhaa@users.noreply.github.com>
2026-03-22 17:08:13 +0530

    Time: 4 ms (96.73%) | Memory: 90.6 MB (40.96%) - LeetSync

--- Company_prep/2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.java
+++ Company_prep/2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.java
@@ -2,7 +2,7 @@ class Solution {
     public int minSwaps(int[] nums) {
         int n = nums.length;
         int total = 0;
-        for(int num : nums) {
+        for(int num: nums) {
             total+=num;
         }
         if(total == 0 || total == n) return 0;
@@ -11,12 +11,12 @@ class Solution {
         int left = 0;
         for(int right = 0;right<n+total;right++) {
             curr+=nums[right%n];
-            if(right-left+1 > total) {
+            if(right-left +1 > total) {
                 curr-=nums[left%n];
                 left++;
             }
-            maxones = Math.max(maxones,curr);
+            maxones = Math.max(maxones, curr);
         }
-        return total - maxones;
+        return total-maxones;
     }
 }
