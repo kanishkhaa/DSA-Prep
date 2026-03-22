a9ea577ec6e37ebcffc022e49eb1b51d9a3059b0
kanishkhaa <151703688+kanishkhaa@users.noreply.github.com>
2026-03-22 16:10:51 +0530

    Time: 0 ms (100.00%) | Memory: 48.1 MB (89.84%) - LeetSync

--- Company_prep/792-binary-search/binary-search.java
+++ Company_prep/792-binary-search/binary-search.java
@@ -7,7 +7,7 @@ class Solution {
             if(nums[mid] == target) {
                 return mid;
             } else if(nums[mid] < target) {
-               left = mid+1;
+                left = mid+1;
             } else {
                 right = mid-1;
             }
