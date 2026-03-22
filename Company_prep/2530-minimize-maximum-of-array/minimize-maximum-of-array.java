4a1bbc19117c629089cec5fd77dccd9e6ffd5d52
kanishkhaa <151703688+kanishkhaa@users.noreply.github.com>
2026-03-22 15:50:38 +0530

    Time: 7 ms (56.81%) | Memory: 80.4 MB (41.88%) - LeetSync

--- Company_prep/2530-minimize-maximum-of-array/minimize-maximum-of-array.java
+++ Company_prep/2530-minimize-maximum-of-array/minimize-maximum-of-array.java
@@ -1,10 +1,10 @@
 class Solution {
     public int minimizeArrayValue(int[] nums) {
-        long sum = 0;
         int ans = 0;
+        long sum = 0;
         for(int i=0;i<nums.length;i++) {
             sum+=nums[i];
-            int avg = (int)Math.ceil((double) sum/(i+1));
+            int avg = (int)Math.ceil((double)sum/(i+1));
             ans = Math.max(ans,avg);
         }
         return ans;
