07afe2363f5c09dd0b958f5431b09e041c46200d
kanishkhaa <151703688+kanishkhaa@users.noreply.github.com>
2026-03-23 08:57:56 +0530

    Time: 1 ms (88.36%) | Memory: 44.1 MB (84.58%) - LeetSync

--- Company_prep/78-subsets/subsets.java
+++ Company_prep/78-subsets/subsets.java
@@ -1,17 +1,19 @@
+import java.util.*;
+
 class Solution {
-    public void helper(int[] nums, int index, List<Integer> curr, List<List<Integer>> res) {
-        if(index == nums.length) {
-            res.add(new ArrayList<>(curr));
-            return;
-        }
-        helper(nums,index+1,curr, res);
-        curr.add(nums[index]);
-        helper(nums, index+1, curr, res);
-        curr.remove(curr.size()-1);
-    }
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        helper(nums,0,new ArrayList<>(),res);
-        return res;
+        List<List<Integer>> result = new ArrayList<>();
+        result.add(new ArrayList<>()); // start with empty subset
+
+        for (int num : nums) {
+            int size = result.size();
+            for (int i = 0; i < size; i++) {
+                List<Integer> newSubset = new ArrayList<>(result.get(i));
+                newSubset.add(num);
+                result.add(newSubset);
+            }
+        }
+
+        return result;
     }
 }
