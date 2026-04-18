d60042ec3e4499b4ed4f45bac1470e6818cdbf8a
Kanishkhaa <151703688+kanishkhaa@users.noreply.github.com>
2026-04-18 08:58:03 +0530

    Time: 14 ms (94.20%) | Memory: 51.4 MB (60.60%) - LeetSync

--- Company_prep/455-assign-cookies/assign-cookies.java
+++ Company_prep/455-assign-cookies/assign-cookies.java
@@ -1,15 +1,15 @@
 class Solution {
     public int findContentChildren(int[] g, int[] s) {
-        int n = g.length;
-        int m = s.length;
+        int m = g.length;
+        int n = s.length;
         Arrays.sort(g);
         Arrays.sort(s);
-        int left = 0,right = 0;
-        while(left<m && right<n) {
+        int left = 0, right = 0;
+        while(left<n && right<m) {
             if(s[left] >= g[right]) {
-                right = right+1;
+                right +=1;
             }
-            left = left+1;
+            left+=1;
         }
         return right;
     }
