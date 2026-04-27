f4c95e0e60b286476bb1a0ebd17fefd80c3cec59
Kanishkhaa <151703688+kanishkhaa@users.noreply.github.com>
2026-04-27 09:16:50 +0530

    Time: 956 ms (48.96%) | Memory: 0B (100.00%) - LeetSync

--- Company_prep/577-employee-bonus/employee-bonus.sql
+++ Company_prep/577-employee-bonus/employee-bonus.sql
@@ -1,2 +1,9 @@
-select e.name, b.bonus from Employee e left join Bonus b on e.empId = b.empId
-where b.bonus < 1000 or b.bonus IS NULL;
+-- select e.name, b.bonus from Employee e left join Bonus b on e.empId = b.empId
+-- where b.bonus < 1000 or b.bonus IS NULL;
+
+select e.name, b.bonus from Employee e 
+left join Bonus b on e.empID = b.empID
+where e.empID NOT IN  (
+    select empID from Bonus 
+)
+OR bonus<1000;
