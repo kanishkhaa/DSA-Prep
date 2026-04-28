cd48f99ff51318fb0c369fd2756f896404dc7baa
Kanishkhaa <151703688+kanishkhaa@users.noreply.github.com>
2026-04-28 09:24:43 +0530

    Time: 364 ms (42.35%) | Memory: 0B (100.00%) - LeetSync

--- Company_prep/1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ Company_prep/1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -1,8 +1,8 @@
 SELECT 
     query_name, 
-    ROUND(AVG(rating * 1.0 / position), 2) AS quality,
+    ROUND(AVG(rating  / position), 2) AS quality,
     ROUND(
-        SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) * 100.0 / COUNT(*),
+        SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) * 100 / COUNT(*),
         2
     ) AS poor_query_percentage
 FROM Queries
