a39c9cb545dfcafe6b63474dc85a73fd877f8c43
Kanishkhaa <151703688+kanishkhaa@users.noreply.github.com>
2026-04-29 10:34:16 +0530

    Time: 2 ms (36.89%) | Memory: 43.1 MB (14.40%) - LeetSync

--- Company_prep/394-decode-string/decode-string.java
+++ Company_prep/394-decode-string/decode-string.java
@@ -7,12 +7,12 @@ class Solution {
         for(char ch : s.toCharArray()) {
             if(Character.isDigit(ch)) {
                 k = k*10+(ch-'0');
-            } else if(ch=='[') {
+            } else if(ch =='[') {
                 count.push(k);
                 st.push(curr);
                 curr = "";
                 k=0;
-            } else if(ch==']') {
+            } else if(ch == ']') {
                 int repeat = count.pop();
                 StringBuilder temp = new StringBuilder(st.pop());
                 for(int i=0;i<repeat;i++) {
