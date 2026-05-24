2dfd3e72f3ff877886575f9616726dad48541f24
Kanishkhaa <151703688+kanishkhaa@users.noreply.github.com>
2026-05-24 18:52:14 +0530

    Time: 16 ms (22.03%) | Memory: 46.5 MB (23.66%) - LeetSync

--- Company_prep/242-valid-anagram/valid-anagram.java
+++ Company_prep/242-valid-anagram/valid-anagram.java
@@ -1,9 +1,17 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        char[] sarr = s.toCharArray();
-        char[] tarr = t.toCharArray();
-        Arrays.sort(sarr);
-        Arrays.sort(tarr);
-        return Arrays.equals(sarr,tarr);
+        if(s.length() != t.length()) return false;
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(char c : t.toCharArray()) {
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+        for(char ch : s.toCharArray()) {
+            if(!map.containsKey(ch)) {
+                return false;
+            }
+            map.put(ch, map.getOrDefault(ch,0)-1);
+            if(map.get(ch) <0) return false; 
+        }
+        return true;
     }
 }
