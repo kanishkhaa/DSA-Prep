8b98d75af3648cc1a191d43693aec6258768d465
Kanishkhaa <151703688+kanishkhaa@users.noreply.github.com>
2026-07-01 19:03:28 +0530

    Time: 15 ms (55.43%) | Memory: 47.5 MB (65.67%) - LeetSync

--- Company_prep/347-top-k-frequent-elements/top-k-frequent-elements.java
+++ Company_prep/347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,20 +1,20 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-     Map<Integer,Integer> map = new HashMap<>();
-     int[] res  = new int[k];
-     for(int i=0;i<nums.length;i++) {
-        map.put(nums[i] , map.getOrDefault(nums[i],0)+1);
-     }   
-     PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> map.get(a) - map.get(b));
-     for(int key : map.keySet()) {
-        pq.add(key);
-        if(pq.size() > k) {
-            pq.poll();
+        Map<Integer,Integer> map = new HashMap<>();
+        int[] res = new int[k];
+        for(int n : nums) {
+            map.put(n,map.getOrDefault(n,0)+1);
         }
-     }
-     for(int i=0;i<k;i++) {
-        res[i] = pq.poll();
-      }
-     return res;
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> map.get(a) - map.get(b));
+        for(int key : map.keySet()) {
+           pq.add(key);
+           if(pq.size() > k) {
+             pq.poll();
+           }
+        }
+        for(int i=0;i<k;i++) {
+            res[i] = pq.poll();
+        }
+        return res;
     }
 }
