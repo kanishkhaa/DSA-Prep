1e4553f1486d9a3cdb3dcfec587a2fbaee23c613
Kanishkhaa <151703688+kanishkhaa@users.noreply.github.com>
2026-07-01 20:52:16 +0530

    Time: 0 ms (100.00%) | Memory: 46.8 MB (61.60%) - LeetSync

---
+++ Company_prep/733-flood-fill/flood-fill.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int original = image[sr][sc];
+        if(original == color) {
+            return image;
+        }
+        dfs(image, sr,sc,original, color);
+        return image;
+    }
+    public void dfs(int[][] image, int sr, int sc, int original, int color) {
+        if(sr <0 || sc<0 || sr>=image.length || sc>= image[0].length) {
+            return;
+        }
+        if(image[sr][sc] != original) {
+            return;
+        }
+        image[sr][sc] = color;
+        dfs(image, sr+1, sc, original, color);
+        dfs(image, sr-1,sc,original, color);
+        dfs(image, sr, sc+1, original, color);
+        dfs(image, sr, sc-1, original, color);
+    }
+}
