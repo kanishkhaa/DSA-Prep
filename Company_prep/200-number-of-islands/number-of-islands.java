9be1f9264807165eae94677d1ec999716e059b69
Kanishkhaa <151703688+kanishkhaa@users.noreply.github.com>
2026-07-01 20:45:25 +0530

    Time: 3 ms (87.56%) | Memory: 51.8 MB (77.67%) - LeetSync

--- Company_prep/200-number-of-islands/number-of-islands.java
+++ Company_prep/200-number-of-islands/number-of-islands.java
@@ -7,20 +7,20 @@ class Solution {
             for(int j=0;j<n;j++) {
                 if(grid[i][j] == '1') {
                     count++;
-                    dfs(grid,i,j,m,n);
+                    dfs(grid, i, j, m, n);
                 }
             }
         }
         return count;
     }
-    public static void dfs(char[][] grid , int i, int j, int m, int n) {
-        if(i<0 || i>=m || j<0 || j>=n || grid[i][j] == '0') {
+    public static void dfs(char[][] grid, int i, int j , int m, int n) {
+        if(i<0 || j<0 || i>=m || j>=n || grid[i][j]=='0') {
             return;
         }
-        grid[i][j] = '0';
-        dfs(grid,i+1,j,m,n);
-        dfs(grid,i-1,j,m,n);
-        dfs(grid,i,j+1,m,n);
-        dfs(grid,i,j-1,m,n);
+        grid[i][j] ='0';
+        dfs(grid,  i+1, j, m, n);
+        dfs(grid, i-1, j, m,n);
+        dfs(grid, i,j+1, m,n);
+        dfs(grid, i,j-1,m, n);
     }
 }
