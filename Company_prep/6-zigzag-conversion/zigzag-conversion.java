83a2ce6363c28587e128440cc422b7162b2a92dd
Kanishkhaa <151703688+kanishkhaa@users.noreply.github.com>
2026-07-01 18:20:05 +0530

    Time: 4 ms (88.36%) | Memory: 46.3 MB (94.63%) - LeetSync

--- Company_prep/6-zigzag-conversion/zigzag-conversion.java
+++ Company_prep/6-zigzag-conversion/zigzag-conversion.java
@@ -1,28 +1,31 @@
 class Solution {
-    public String convert(String s, int numRows) {
-        if (numRows == 1 || s.length() <= numRows) return s;
-
-        StringBuilder[] rows = new StringBuilder[numRows];
-        for (int i = 0; i < numRows; i++) {
-            rows[i] = new StringBuilder();
+    public String convert(String s, int rows) {
+        if(rows == 1 || s.length() <= rows) {
+            return s;
         }
-
-        int currentRow = 0;
-        boolean goingDown = false;
-
-        for (char c : s.toCharArray()) {
-            rows[currentRow].append(c);
-            if (currentRow == 0 || currentRow == numRows - 1) {
-                goingDown = !goingDown;
+        StringBuilder[] res = new StringBuilder[rows];
+        int r = 0;
+        for(int i=0;i<rows;i++) {
+            res[i] = new StringBuilder();
+        }
+        boolean down = true;
+        for(char ch : s.toCharArray()) {
+            res[r].append(ch);
+            if(r == 0) {
+                down = true;
+            } else if(r == rows-1) {
+                down = false;
             }
-            currentRow += goingDown ? 1 : -1;
+            if(down) {
+                r++;
+            } else {
+                r--;
+            }
         }
-
-        StringBuilder result = new StringBuilder();
-        for (StringBuilder row : rows) {
-            result.append(row);
+        StringBuilder ans = new StringBuilder();
+        for(StringBuilder sb : res) {
+            ans.append(sb);
         }
-
-        return result.toString();
+        return ans.toString();
     }
 }
