17e5f3c9b2b3ab3fdea3d568b06ef440634eb11f
Kanishkhaa <151703688+kanishkhaa@users.noreply.github.com>
2026-07-06 10:51:59 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (18.40%) - LeetSync

--- Company_prep/198-house-robber/house-robber.java
+++ Company_prep/198-house-robber/house-robber.java
@@ -1,17 +1,13 @@
 class Solution {
     public int rob(int[] nums) {
-        if(nums.length==1) return nums[0];
-        int sum = 0;
-        int prev = nums[0];
-        int prev2 = 0;
-        for(int i=1;i<nums.length;i++) {
-            int include = nums[i];
-            if(i>1) include+=prev2;
-            int exclude = prev;
-            int curr = Math.max(include,exclude);
-            prev2 = prev;
-            prev = curr;
+        int n = nums.length;
+        if(n==1)  return nums[0];
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0] , nums[1]);
+        for(int i=2;i<n;i++) {
+            dp[i] = Math.max(dp[i-1] ,nums[i] + dp[i-2]);
         }
-        return prev;
+        return dp[n-1];
     }
 }
