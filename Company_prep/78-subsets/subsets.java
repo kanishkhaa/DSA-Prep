4ac37d54c752b2da36742ef174d722f8d01cd819
Kanishkhaa <151703688+kanishkhaa@users.noreply.github.com>
2026-07-09 08:52:23 +0530

    Time: 1 ms (87.47%) | Memory: 44.4 MB (7.51%) - LeetSync

--- Company_prep/78-subsets/subsets.java
+++ Company_prep/78-subsets/subsets.java
@@ -1,10 +1,7 @@
-import java.util.*;
-
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        result.add(new ArrayList<>()); // start with empty subset
-
+        result.add(new ArrayList<>()); 
         for (int num : nums) {
             int size = result.size();
             for (int i = 0; i < size; i++) {
@@ -13,7 +10,6 @@ class Solution {
                 result.add(newSubset);
             }
         }
-
         return result;
     }
 }
