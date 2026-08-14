92adca0b575ca4cc8a17b19400bd575acba88b50
Kanishkhaa <151703688+kanishkhaa@users.noreply.github.com>
2026-08-14 10:15:04 +0530

    Time: 7 ms (46.61%) | Memory: 80.3 MB (60.63%) - LeetSync

--- Company_prep/2530-minimize-maximum-of-array/minimize-maximum-of-array.java
+++ Company_prep/2530-minimize-maximum-of-array/minimize-maximum-of-array.java
@@ -5,7 +5,7 @@ class Solution {
         for(int i=0;i<nums.length;i++) {
             sum+=nums[i];
             int avg = (int)Math.ceil((double)sum/(i+1));
-            ans = Math.max(ans,avg);
+            ans = Math.max(avg,ans);
         }
         return ans;
     }
